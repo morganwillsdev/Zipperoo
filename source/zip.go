c1c6ee005ecd8a0e71c4f1cdc1fcd44d21572794
morganwillsdev <mogz1@yahoo.com>
2019-08-20 23:54:23 +0100

    reorganised files

---
+++ source/zip.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+    "archive/zip"
+    "io"
+    "os"
+)
+
+
+func ZipFiles(filename string, files []string) error {
+
+    newZipFile, err := os.Create(filename)
+    if err != nil {
+        return err
+    }
+    defer newZipFile.Close()
+
+    zipWriter := zip.NewWriter(newZipFile)
+    defer zipWriter.Close()
+
+    // Add files to zip
+    for _, file := range files {
+        if err = addFileToZip(zipWriter, file); err != nil {
+            return err
+        }
+    }
+    return nil
+}
+
+
+func addFileToZip(zipWriter *zip.Writer, filename string) error {       // CHANGE TO WORK WITH FOLDERS
+
+    fileToZip, err := os.Open(filename)
+    if err != nil {
+        return err
+    }
+    defer fileToZip.Close()
+
+    // Get the file information
+    info, err := fileToZip.Stat()
+    if err != nil {
+        return err
+    }
+
+    header, err := zip.FileInfoHeader(info)
+    if err != nil {
+        return err
+    }
+
+    header.Name = filename
+    header.Method = zip.Deflate
+
+    writer, err := zipWriter.CreateHeader(header)
+    if err != nil {
+        return err
+    }
+    _, err = io.Copy(writer, fileToZip)
+    return err
+}
